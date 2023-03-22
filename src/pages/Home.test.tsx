d7f2bea2e5a2e00ee9327d489eee463a05119b54
Eugene Mikhalionok <eugenemikhalionok@gmail.com>
2023-03-22 14:19:51 +0300

    feat: setup vitest

---
+++ src/pages/Home.test.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import { Home } from './home';
+
+describe('Home', () => {
+  it('Render Home Page', () => {
+    render(<Home />);
+
+    expect(screen.getByRole('heading', {
+      level: 1
+    })).toHaveTextContent('Home Page');
+  });
+});
